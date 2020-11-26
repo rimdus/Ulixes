6ac8b2077a7c38521474f94d354310b15ae02a20
rimdus <rimdus@yandex.ru>
2020-11-26 16:00:31 +0700

    init

---
+++ src/decorators/decorator.helper.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import { DecoratorHelper } from './decorator.helper';
+
+describe('decorator.helper', () => {
+  context('methods', () => {
+    it('should options be equal', () => {
+      class DHCCls {
+      }
+
+      class DHCClsHlp {
+      }
+
+      DecoratorHelper.defineInjectOptions(
+        DHCCls,
+        { alias: 'DHCClsAlias', providers: [{ provide: DHCClsHlp, useClass: DHCClsHlp }] },
+      );
+      const dhOpt = DecoratorHelper.getInjectionOptions(DHCCls);
+      expect(dhOpt.alias).equal('DHCClsAlias');
+    });
+  });
+});
